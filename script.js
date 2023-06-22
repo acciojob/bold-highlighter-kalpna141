287f82edacb975fba8462de8705fba31db8086d3
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-22 10:10:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let el=document.getElementByTagName("strong");
-function highlight() {
+let el = document.getElementByTagName("strong");
+function highlight()
+	{
     //Write your code here
 	for(let i=0; i<el.length-1; i++)
 		{
-			el[i].style.color="green";
+			el[i].style.color = "green";
 		}
-}
+    }
 
-function return_normal() {
+function return_normal() 
+{
     //Write your code here
 	for(let i=0; i<el.length-1; i++)
 		{
-			el[i].style.color="black";
-		}
-
-    
+			el[i].style.color = "black";
+		}  
 }
